3ff0c6f2be0b9ae7fec5b55680a66c7afe586463
Okayrug93 <62489234+Okayrug93@users.noreply.github.com>
2024-03-20 18:03:04 +0100

    Fixed imports and formatting

--- src/commands/zoo.command/zoo.data.ts
+++ src/commands/zoo.command/zoo.data.ts
@@ -1,13 +1,13 @@
-import { SlashCommandBuilder } from 'discord.js';
-import { PermissionFlagsBits } from 'discord.js';
+import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
 
 export const data = new SlashCommandBuilder()
     .setName('troll-zoo')
     .setDescription('Place the provided user in the zoo! (beware, removes all their roles)')
     .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers)
-    .addUserOption(
-        (option) =>  
-            option.setName('newly-zood')
-                .setDescription('The person you want to have locked up in the zoo')
-                .setRequired(true))
+    .addUserOption((option) =>
+        option
+            .setName('newly-zood')
+            .setDescription('The person you want to have locked up in the zoo')
+            .setRequired(true)
+    )
     .toJSON();
